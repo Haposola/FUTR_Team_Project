f4c9255b05c2c75bdc4c8c4510254e9e7bf57b85
Haposola <haposola@gmail.com>
2014-11-18 18:29:34 +0800

    change config try to mail

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,19 @@ end
 
 module Team
   class Application < Rails::Application
+    config.action_mailer.raise_delivery_errors = true     #注意，在development.rb下需修改成true
+    #添加如下几行代码
+    config.action_mailer.default_url_options = { :host => "localhost:3000" } #提示中有提到需要配置，即执行rails g devise:install
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address:              'smtp.gmail.com',
+        port:                 587,
+        domain:               'gmail.com',
+        user_name:            'haposola@gmail.com',
+        password:             'gg.19950609',
+        authentication:       :login,
+        enable_starttls_auto: true
+  } 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
