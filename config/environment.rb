552dd7b6ee6b9a47f0bd807bddcc9b85d1cd0b63
Haposola <haposola@gmail.com>
2014-11-18 15:48:08 +0800

    add some attrs to user

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Team::Application.initialize!
+ActionMailer::Base.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+    	address:              'smtp.gmail.com',
+    	port:                 587,
+    	domain:               'gmail.com',
+    	user_name:            'haposola@gmail.com',
+    	password:             'gg.19950609',
+    	authentication:       'plain',
+    	enable_starttls_auto: true
+    }
+    ActionMailer::Base.perform_deliveries = true
+    ActionMailer::Base.raise_delivery_errors = true
--- db/migrate/20141118064207_devise_create_users.rb
+++ db/migrate/20141118064207_devise_create_users.rb
@@ -4,7 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      ##User info
+      t.string :nickname
+      t.string :name
+      t.string :nation
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -19,11 +22,11 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :current_sign_in_ip
       t.string   :last_sign_in_ip
 
-      ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      # Confirmable
+       t.string   :confirmation_token
+       t.datetime :confirmed_at
+       t.datetime :confirmation_sent_at
+       t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
@@ -36,7 +39,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
