63af3408e9937015fbeb40cdee561ace17b9de84
Haposola <haposola@gmail.com>
2014-11-21 21:34:16 +0800

    backend a little change

--- db/migrate/20141118064207_devise_create_users.rb
+++ db/migrate/20141118064207_devise_create_users.rb
@@ -8,8 +8,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :nickname
       t.string :name
       t.string :nation
-      t.string :category, default: "commen"
-      t.string :realconfirm,default: "yes"
+      t.string :category
+      t.string :realconfirm
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
